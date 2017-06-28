0f5931a62f4c81a30dfad6807c08eaf37000d18c
xiaohuajun <xiaohuajun@bilibili.com>
2017-06-28 10:43:11 +0800

    template completed

--- meta.js
+++ meta.js
@@ -2,10 +2,10 @@ module.exports = {
   "helpers": {
     "if_or": function (v1, v2, options) {
       if (v1 || v2) {
-        return options.fn(this);
+        return options.fn(this)
       }
 
-      return options.inverse(this);
+      return options.inverse(this)
     }
   },
   "prompts": {
@@ -90,4 +90,4 @@ module.exports = {
   },
   "completeMessage": "completed"
   // "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
-};
+}
--- template/src/App.vue
+++ template/src/App.vue
@@ -13,6 +13,7 @@ export default {
   components: {
     Hello
   }
+}
 </script>
 
 <style>
